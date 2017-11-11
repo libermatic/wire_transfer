a8d9851101b24c02a63f432060b9cb6ffbde0137
Sun Howwrongbum <sun@adrenosapient.com>
2017-11-12 02:38:13 +0530

    Add GL Entry operation on Wire Transaction submit

--- wire_transfer/wire_transfer/doctype/wire_transaction/wire_transaction.js
+++ wire_transfer/wire_transfer/doctype/wire_transaction/wire_transaction.js
@@ -2,7 +2,39 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Wire Transaction', {
-	refresh: function(frm) {
-
-	}
+	'onload': function(frm, dt, dn) {
+		if (frm.doc.__islocal) {
+			frappe.call({
+		    method: 'frappe.client.get_value',
+		    args: {
+	        'doctype': 'Wire Transfer Settings',
+	        'fieldname': [
+						'default_debit_account',
+						'default_credit_account',
+            'default_income_account',
+						'default_service_rate',
+	        ]
+		    },
+		    callback: function(r) {
+	        if (!r.exc) {
+						const { message : {
+							default_debit_account, default_credit_account, default_income_account, default_service_rate,
+						} } = r;
+						frm.set_value('debit_account', default_debit_account);
+						frm.set_value('credit_account', default_credit_account);
+						frm.set_value('income_account', default_income_account);
+	          frm.set_value('service_rate', default_service_rate);
+	        }
+		    }
+			});
+		}
+	},
+	'amount': function (frm, dt, dn) {
+		frm.set_value('charges', frm.doc['amount'] * frm.doc['service_rate'] / 100.0);
+		frm.set_value('total', frm.doc['amount'] + frm.doc['charges']);
+	},
+	'charges': function (frm, dt, dn) {
+		frm.set_value('service_rate', frm.doc['charges'] / frm.doc['amount'] * 100.0);
+		frm.set_value('total', frm.doc['amount'] + frm.doc['charges']);
+	},
 });
--- wire_transfer/wire_transfer/doctype/wire_transaction/wire_transaction.py
+++ wire_transfer/wire_transfer/doctype/wire_transaction/wire_transaction.py
@@ -4,7 +4,46 @@
 
 from __future__ import unicode_literals
 import frappe
-from frappe.model.document import Document
+from erpnext import get_default_company
+from erpnext.accounts.utils import get_account_currency
+from erpnext.accounts.general_ledger import make_gl_entries
+from erpnext.controllers.accounts_controller import AccountsController
 
-class WireTransaction(Document):
-	pass
+class WireTransaction(AccountsController):
+	def on_submit(self):
+		self.set_missing_fields()
+		self.make_gl_entries()
+
+	def on_cancel(self):
+		self.set_missing_fields()
+		self.make_gl_entries(cancel=1)
+
+	def set_missing_fields(self):
+		self.posting_date = self.transaction_date
+		self.company = get_default_company()
+
+	def make_gl_entries(self, cancel=0, adv_adj=0):
+		gl_entries = [
+			self.get_gl_dict({
+					'account': self.debit_account,
+					'account_currency': get_account_currency(self.debit_account),
+					'debit_in_account_currency': self.amount,
+					'debit': self.amount,
+					'remarks': "{}".format(self.to_account),
+				}),
+			self.get_gl_dict({
+					'account': self.income_account,
+					'account_currency': get_account_currency(self.income_account),
+					'debit_in_account_currency': self.charges,
+					'debit': self.charges,
+					'cost_center': frappe.db.get_value('Company', self.company, 'cost_center')
+				}),
+			self.get_gl_dict({
+					'account': self.credit_account,
+					'account_currency': get_account_currency(self.credit_account),
+					'credit_in_account_currency': self.total,
+					'credit': self.total,
+					'remarks': "{}".format(self.customer),
+				}),
+		]
+		make_gl_entries(gl_entries, cancel=cancel, adv_adj=adv_adj)
