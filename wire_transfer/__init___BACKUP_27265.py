db2430a5e6df0a19d2766ba5e964b26f74e95802
Sun Howwrongbum <sun@adrenosapient.com>
2017-11-13 00:55:31 +0530

    Remove merge artifacts

--- wire_transfer/__init___BACKUP_27265.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-<<<<<<< HEAD
-__version__ = '0.1.0'
-=======
-__version__ = '0.0.2'
->>>>>>> dc06c188ec980e48782d45728744a221597e3806
