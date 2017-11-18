6b9e85bd8b03b43120a67a676a7e4f37cdf86552
Sun Howwrongbum <sun@adrenosapient.com>
2017-11-18 14:14:42 +0530

    Update Wire Transaction script with es6 pattern

--- wire_transfer/wire_transfer/doctype/wire_transaction/wire_transaction.js
+++ wire_transfer/wire_transfer/doctype/wire_transaction/wire_transaction.js
@@ -2,40 +2,45 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Wire Transaction', {
-	'onload': function(frm, dt, dn) {
-		if (frm.doc.__islocal) {
-			frm.set_value('company', frappe.defaults.get_default('Company'));
-			frappe.call({
-		    method: 'frappe.client.get_value',
-		    args: {
-	        'doctype': 'Wire Transfer Settings',
-	        'fieldname': [
-						'default_debit_account',
-						'default_credit_account',
-            'default_income_account',
-						'default_service_rate',
-	        ]
-		    },
-		    callback: function(r) {
-	        if (!r.exc) {
-						const { message : {
-							default_debit_account, default_credit_account, default_income_account, default_service_rate,
-						} } = r;
-						frm.set_value('debit_account', default_debit_account);
-						frm.set_value('credit_account', default_credit_account);
-						frm.set_value('income_account', default_income_account);
-	          frm.set_value('service_rate', default_service_rate);
-	        }
-		    }
-			});
-		}
-	},
-	'amount': function (frm, dt, dn) {
-		frm.set_value('charges', frm.doc['amount'] * frm.doc['service_rate'] / 100.0);
-		frm.set_value('total', frm.doc['amount'] + frm.doc['charges']);
-	},
-	'charges': function (frm, dt, dn) {
-		frm.set_value('service_rate', frm.doc['charges'] / frm.doc['amount'] * 100.0);
-		frm.set_value('total', frm.doc['amount'] + frm.doc['charges']);
-	},
+  onload: async function(frm) {
+    if (frm.doc.__islocal) {
+      frm.set_value('company', frappe.defaults.get_default('Company'));
+      const { message } = await frappe.db.get_value(
+        'Wire Transfer Settings',
+        null,
+        [
+          'default_debit_account',
+          'default_credit_account',
+          'default_income_account',
+          'default_service_rate',
+        ]
+      );
+      if (message) {
+        const {
+          default_debit_account,
+          default_credit_account,
+          default_income_account,
+          default_service_rate,
+        } = message;
+        frm.set_value('debit_account', default_debit_account);
+        frm.set_value('credit_account', default_credit_account);
+        frm.set_value('income_account', default_income_account);
+        frm.set_value('service_rate', default_service_rate);
+      }
+    }
+  },
+  amount: async function(frm) {
+    frm.set_value(
+      'charges',
+      frm.doc['amount'] * frm.doc['service_rate'] / 100.0
+    );
+    frm.set_value('total', frm.doc['amount'] + frm.doc['charges']);
+  },
+  charges: async function(frm) {
+    frm.set_value(
+      'service_rate',
+      frm.doc['charges'] / frm.doc['amount'] * 100.0
+    );
+    frm.set_value('total', frm.doc['amount'] + frm.doc['charges']);
+  },
 });
