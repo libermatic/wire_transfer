713b1e126e7711339c6ac06e07461deca1b33860
Sun Howwrongbum <sun@adrenosapient.com>
2017-12-06 05:05:27 +0530

    Add feature to handle Customer receivables

--- wire_transfer/wire_transfer/doctype/wire_transaction/wire_transaction.js
+++ wire_transfer/wire_transfer/doctype/wire_transaction/wire_transaction.js
@@ -2,9 +2,112 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Wire Transaction', {
+  refresh: async function(frm) {
+    if (
+      frm.doc['docstatus'] === 1 &&
+      0 < frm.doc['paid_amount'] &&
+      frm.doc['paid_amount'] < frm.doc['total']
+    ) {
+      const remove_dialog = new frappe.ui.Dialog({
+        title: 'Delete Payment',
+        fields: [{ fieldname: 'ht', fieldtype: 'HTML' }],
+      });
+      const container = $('<table />').addClass(
+        'table table-condensed table-striped'
+      );
+      container.append(
+        $('<tr />')
+          .append($('<th scope="col" />').text('Date'))
+          .append($('<th scope="col" class="text-right" />').text('Amount'))
+          .append($('<th scope="col" class="text-center" />').text('Action'))
+      );
+      remove_dialog.fields_dict.ht.$wrapper.append(container);
+      frm.doc['payments'].forEach(function({
+        payment_id,
+        payment_date,
+        payment_amount,
+      }) {
+        const handle_click = async function() {
+          const { message = {} } = await frappe.call({
+            method:
+              'wire_transfer.wire_transfer.doctype.wire_transaction.wire_transaction.make_payment',
+            args: {
+              name: frm.doc['name'],
+              payment_id,
+              payment_date,
+              payment_amount,
+              reverse: 1,
+            },
+          });
+          remove_dialog.hide();
+          frm.reload_doc();
+        };
+        container.append(
+          $('<tr />')
+            .append($('<td />').text(payment_date))
+            .append(
+              $('<td class="text-right" />').text(
+                format_currency(
+                  payment_amount,
+                  frappe.defaults.get_default('currency'),
+                  2
+                )
+              )
+            )
+            .append(
+              $('<td class="text-center" />').append(
+                $('<i class="fa fa-times" style="cursor: pointer;" />').click(
+                  handle_click
+                )
+              )
+            )
+        );
+      });
+      frm.add_custom_button(__('Delete Payment'), async function(fields) {
+        remove_dialog.show();
+      });
+    }
+    if (frm.doc['docstatus'] === 1 && !frm.doc['is_paid']) {
+      const pay_dialog = new frappe.ui.Dialog({
+        title: 'Make Payment',
+        fields: [
+          {
+            label: 'Date',
+            fieldname: 'payment_date',
+            fieldtype: 'Date',
+            default: frappe.datetime.now_date(),
+          },
+          {
+            label: 'Amount',
+            fieldname: 'payment_amount',
+            fieldtype: 'Currency',
+            default: frm.doc['total'] - frm.doc['paid_amount'],
+          },
+        ],
+      });
+      pay_dialog.set_primary_action(__('Pay'), async function(fields) {
+        await frappe.call({
+          method:
+            'wire_transfer.wire_transfer.doctype.wire_transaction.wire_transaction.make_payment',
+          args: Object.assign(
+            { name: frm.doc['name'], payment_id: frappe.utils.get_random(10) },
+            fields
+          ),
+        });
+        this.hide();
+        frm.reload_doc();
+      });
+      const pay_button = frm.add_custom_button(
+        __('Receive Payment'),
+        function() {
+          pay_dialog.show();
+        }
+      );
+      pay_button.addClass('btn-primary');
+    }
+  },
   onload: async function(frm) {
     if (frm.doc.__islocal) {
-      frm.set_value('company', frappe.defaults.get_default('Company'));
       const { message } = await frappe.db.get_value(
         'Wire Transfer Settings',
         null,
@@ -22,8 +125,8 @@ frappe.ui.form.on('Wire Transaction', {
           default_income_account,
           default_service_rate,
         } = message;
-        frm.set_value('debit_account', default_debit_account);
-        frm.set_value('credit_account', default_credit_account);
+        frm.set_value('debit_to', default_debit_account);
+        frm.set_value('credit_from', default_credit_account);
         frm.set_value('income_account', default_income_account);
         frm.set_value('service_rate', default_service_rate);
       }
@@ -37,10 +140,20 @@ frappe.ui.form.on('Wire Transaction', {
     frm.set_value('total', frm.doc['amount'] + frm.doc['charges']);
   },
   charges: async function(frm) {
-    frm.set_value(
-      'service_rate',
-      frm.doc['charges'] / frm.doc['amount'] * 100.0
-    );
     frm.set_value('total', frm.doc['amount'] + frm.doc['charges']);
+  },
+  is_paid: async function(frm) {
+    if (frm.doc['is_paid']) {
+      frm.set_value('paid_amount', frm.doc['total']);
+    } else {
+      frm.set_value('paid_amount', 0);
+    }
+  },
+  total: async function(frm) {
+    const { total, amount } = frm.doc;
+    frm.set_value('charges', total - amount);
+    if (frm.doc['is_paid']) {
+      frm.set_value('paid_amount', total);
+    }
   },
 });
--- wire_transfer/wire_transfer/doctype/wire_transaction/wire_transaction.py
+++ wire_transfer/wire_transfer/doctype/wire_transaction/wire_transaction.py
@@ -4,44 +4,142 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
+from frappe.utils import flt, random_string
+from erpnext import get_default_company
+from erpnext.accounts.party import get_party_account
 from erpnext.accounts.utils import get_account_currency
 from erpnext.accounts.general_ledger import make_gl_entries
 from erpnext.controllers.accounts_controller import AccountsController
 
 class WireTransaction(AccountsController):
+	def before_submit(self):
+		if self.is_paid:
+			self.append('payments', {
+					'payment_id': random_string(10),
+					'payment_date': self.transaction_date,
+					'payment_amount': self.paid_amount
+				})
+
 	def on_submit(self):
 		self.set_missing_fields()
-		self.make_gl_entries()
+		self.make_parent_gl_entries()
 
 	def on_cancel(self):
 		self.set_missing_fields()
-		self.make_gl_entries(cancel=1)
+		for entry in self.payments:
+				make_payment(
+				self.name,
+				payment_id=entry.payment_id,
+				payment_date=entry.payment_date,
+				payment_amount=entry.payment_amount,
+				parent_cancel=1
+			)
+		self.make_parent_gl_entries(cancel=1)
+
+	def before_update_after_submit(self):
+		total_paid = 0.0
+		for row in self.payments:
+			total_paid += flt(row.get_value('payment_amount'))
+			if total_paid > self.total:
+				return frappe.throw(_("Total paid amount cannot be greater than invoiced amount"))
+		self.paid_amount = total_paid
+		if total_paid == self.total:
+			self.is_paid = 1
 
 	def set_missing_fields(self):
 		self.posting_date = self.transaction_date
+		self.company = get_default_company()
 
-	def make_gl_entries(self, cancel=0, adv_adj=0):
+	def make_parent_gl_entries(self, cancel=0, adv_adj=0):
+		party_account = get_party_account('Customer', self.customer, self.company)
 		gl_entries = [
 			self.get_gl_dict({
-					'account': self.debit_account,
-					'account_currency': get_account_currency(self.debit_account),
-					'debit_in_account_currency': self.amount,
-					'debit': self.amount,
-					'remarks': "{}".format(self.to_account),
+					'account': self.credit_from,
+					'account_currency': get_account_currency(self.credit_from),
+					'credit_in_account_currency': self.amount,
+					'credit': self.amount,
+					'against': self.to_account,
 				}),
 			self.get_gl_dict({
 					'account': self.income_account,
 					'account_currency': get_account_currency(self.income_account),
-					'debit_in_account_currency': self.charges,
-					'debit': self.charges,
-					'cost_center': frappe.db.get_value('Company', self.company, 'cost_center')
+					'credit_in_account_currency': self.charges,
+					'credit': self.charges,
+					'cost_center': frappe.db.get_value('Company', self.company, 'cost_center'),
+					'against': self.customer,
 				}),
 			self.get_gl_dict({
-					'account': self.credit_account,
-					'account_currency': get_account_currency(self.credit_account),
-					'credit_in_account_currency': self.total,
-					'credit': self.total,
-					'remarks': "{}".format(self.customer),
+					'account': party_account,
+					'account_currency': get_account_currency(party_account),
+					'debit_in_account_currency': self.total,
+					'debit': self.total,
+					'party_type': 'Customer',
+					'party': self.customer,
 				}),
 		]
+		if self.is_paid:
+			gl_entries.append(self.get_gl_dict({
+					'account': party_account,
+					'account_currency': get_account_currency(party_account),
+					'credit_in_account_currency': self.total,
+					'credit': self.total,
+					'party_type': 'Customer',
+					'party': self.customer,
+					'against': self.debit_to,
+				}))
+			gl_entries.append(self.get_gl_dict({
+					'account': self.debit_to,
+					'account_currency': get_account_currency(self.debit_to),
+					'debit_in_account_currency': self.total,
+					'debit': self.total,
+					'against': self.customer,
+				}))
 		make_gl_entries(gl_entries, cancel=cancel, adv_adj=adv_adj)
+
+
+@frappe.whitelist()
+def make_payment(name, payment_id, payment_date, payment_amount, reverse=0, parent_cancel=0):
+	r = frappe.get_doc('Wire Transaction', name)
+	cancel = reverse or parent_cancel
+	if cancel:
+		for entry in r.payments:
+			if entry.get_value('payment_id') == payment_id:
+				r.remove(entry)
+				break
+	else:
+		r.append('payments', {
+				'payment_id': payment_id,
+				'payment_date': payment_date,
+				'payment_amount': payment_amount
+			})
+	if not parent_cancel:
+		r.save()
+	r.posting_date = payment_date
+	r.company = get_default_company()
+	party_account = get_party_account('Customer', r.customer, r.company)
+	gl_entries = [
+		r.get_gl_dict({
+				'account': party_account,
+				'account_currency': get_account_currency(party_account),
+				'credit_in_account_currency': payment_amount,
+				'credit': payment_amount,
+				'voucher_type': 'Wire Transaction Payment',
+				'voucher_no': payment_id,
+				'party_type': 'Customer',
+				'party': r.customer,
+				'against': r.debit_to,
+				'against_voucher_type': 'Wire Transaction',
+				'against_voucher': name,
+			}),
+		r.get_gl_dict({
+				'account': r.debit_to,
+				'account_currency': get_account_currency(r.debit_to),
+				'debit_in_account_currency': payment_amount,
+				'debit': payment_amount,
+				'voucher_type': 'Wire Transaction Payment',
+				'voucher_no': payment_id,
+				'against': r.customer,
+			}),
+	]
+	make_gl_entries(gl_entries, cancel=cancel, adv_adj=0)
