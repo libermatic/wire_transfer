38215f9a0a7ec959a5414f5418ce683ba4e22bde
Sun Howwrongbum <sun@adrenosapient.com>
2017-12-06 18:47:19 +0530

    Update Wire Transaction Register with more fields

--- wire_transfer/wire_transfer/report/wire_transaction_register/wire_transaction_register.py
+++ wire_transfer/wire_transfer/report/wire_transaction_register/wire_transaction_register.py
@@ -8,10 +8,13 @@ from frappe.utils.data import add_days
 
 def execute(filters=None):
 	column = [
-			_("Transaction Date") + ":Date:150",
+			_("Transaction Date") + ":Date:90",
 			_("Document ID") + ":Link/Wire Transaction:90",
-			_("Customer") + ":Link/Customer:120",
 			_("Target Account") + ":Link/Wire Account:120",
+			_("Account Holder") + ":Link/Wire Account:120",
+			_("Customer") + ":Link/Customer:120",
+			_("ID Type") + ":Link/Customer:90",
+			_("ID No") + ":Link/Customer:90",
 			_("Amount") + ":Currency/currency:90",
 			_("Charges") + ":Currency/currency:90",
 			_("Total") + ":Currency/currency:90",
@@ -26,8 +29,20 @@ def execute(filters=None):
 		if filters.get('customer'):
 			cond.append("wt.customer = '%s'" % filters.get('customer'))
 	data = frappe.db.sql("""
-		SELECT wt.transaction_date, wt.name, wt.customer, wt.to_account, wt.amount, wt.charges, wt.total, wt.total-wt.paid_amount
+		SELECT
+			wt.transaction_date,
+			wt.name,
+			wt.to_account,
+			wt.account_holder,
+			wt.customer,
+			ct.id_type,
+			ct.id_no,
+			wt.amount,
+			wt.charges,
+			wt.total,
+			wt.total-wt.paid_amount
 		FROM `tabWire Transaction` AS wt
+		LEFT JOIN `tabCustomer` AS ct ON wt.customer = ct.name
 		WHERE {}
 		ORDER BY wt.transaction_date
 	""".format(" and ".join(cond)))
