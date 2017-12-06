6ac512b903cfad7de055fa48c9855ead100bddab
Sun Howwrongbum <sun@adrenosapient.com>
2017-12-06 18:47:54 +0530

    Wire Account: Add a button to make a Wire Transaction

--- wire_transfer/wire_transfer/doctype/wire_account/wire_account.js
+++ wire_transfer/wire_transfer/doctype/wire_account/wire_account.js
@@ -2,7 +2,11 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Wire Account', {
-	refresh: function(frm) {
-
-	}
+  refresh: function(frm) {
+    if (!frm.doc.__islocal) {
+      frm.add_custom_button(__('Make Wire Transaction'), function() {
+        frm.make_new('Wire Transaction');
+      });
+    }
+  },
 });
